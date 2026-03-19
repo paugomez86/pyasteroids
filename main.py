6fcb78c4ea8498b060a804d311a8e5b8080a23be
paugomez86 <paugomez86@gmail.com>
2026-03-19 03:25:28 +0100

    Player done

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Initializing game
     pygame.init()
+    
+    # Setting screen mode, clock and delta time
+    # dt is the time between frames
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Defining pygame groups 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # Adding classes to pygame groups
+    Player.containers = (updatable, drawable)
+    
+    # Initializing player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -15,8 +32,21 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_TAB]:
+            print(player.__repr__())
+        if keys[pygame.K_ESCAPE]:
+            return
+                
         screen.fill("black")
+        
+        updatable.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
+        dt = clock.tick(MAX_FPS) / 1000
 
 if __name__ == "__main__":
     main()
