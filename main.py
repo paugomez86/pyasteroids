19acd5f6354f5a6870677c2edc6ec1fdefcacd55
paugomez86 <paugomez86@gmail.com>
2026-03-24 01:02:54 +0100

    Added shots and collision

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -23,11 +25,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     # Adding classes to pygame groups
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     
     # Initializing player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -35,26 +39,54 @@ def main():
     # Initializing asteroid field. The object itself will spawn multiple asteroid objects
     asteroidfield = AsteroidField()
 
+    # Game loop. 60 iterations per second (default). In each iteration, the game updates and redraws all the sprites
     while True:
+        # Logger script. For development purposes
         log_state()
+        
+        # Kill the game on window close
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # In-game utility keys
         keys = pygame.key.get_pressed()
         if keys[pygame.K_TAB]:
             print(player.__repr__())
         if keys[pygame.K_ESCAPE]:
             return
-                
+        
+        # Paint the screen background in black
         screen.fill("black")
         
+        # Updating updatable sprites
         updatable.update(dt)
         
+        # Drawing drawable sprites
         for item in drawable:
             item.draw(screen)
+        
+        # Checking for asteroid collisions
+        for asteroid in asteroids:
+            # Collision with player
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
             
+            # Collision with shots
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+                    
+        
+        # Updating the surface object to the screen
         pygame.display.flip()
+        
+        # Setting delta time. Time between FPS (60 by default). clock.tick() gets the number of desired FPS as argument. 
+        # Dividing by 1000 to get it in secons
         dt = clock.tick(MAX_FPS) / 1000
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SCREEN_HEIGHT, SCREEN_WIDTH
+from shot import Shot
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_radius = 1
+        self.shot_cooldown = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -19,8 +22,13 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
         
     def update(self, dt):
+        # Catch the pressed keys
         keys = pygame.key.get_pressed()
+        
+        # Reducing the shot cooldown
+        self.shot_cooldown -= dt
 
+        # Checking keys for player controls
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
@@ -29,6 +37,10 @@ class Player(CircleShape):
             self.rotate(dt * -1)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        # Only shoot in case the cooldown timer is set to 0
+        if keys[pygame.K_SPACE] and not self.shot_cooldown > 0:
+            self.shoot()
+            self.shot_cooldown = PLAYER_SHOT_COOLDOWN_SECONDS
         
         # Added: When player ship reaches the edge of the screen, it pops up in the opposite edge   
         if self.position[0] <= 0:
@@ -48,6 +60,12 @@ class Player(CircleShape):
         
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def shoot(self):
+        shot_vector = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot_vector = self.position + shot_vector * self.radius
+        shot = Shot(shot_vector.x, shot_vector.y, self.shot_radius)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         
     def __repr__(self):
         return f"Player position: {self.position}", f"Player rotation: {self.rotation}"
